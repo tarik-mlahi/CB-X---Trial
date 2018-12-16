9a48cf604263bc9bc4d7855b347d3b82d22cbbd1
ADMIN <tarik.mlahi@persistent.com>
2018-12-16 12:08:34 +0100

    Test sans RTC !!!

--- GS-BusinessLogic/src/logic/Finder.java
+++ GS-BusinessLogic/src/logic/Finder.java
@@ -8,7 +8,7 @@
  *******************************************************************************/
 
 package com.jke.logic;
-//helloworld123456
+//helloworld123456678
 import java.io.File;
 import java.io.IOException;
 import java.net.JarURLConnection;
